c297026c8feafbc9d0dcdfbcbbe6f8e4bd5dbcdc
tbkpro@gmail.com <tbkpro@gmail.com>
2015-11-23 09:47:57 +0700

    Initial commit

---
+++ app/src/androidTest/java/com/example/quang11t1/locationnote/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.quang11t1.locationnote;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
